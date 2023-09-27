daa6728349e92a9476d462c1a41637f32d68fc6e
JuanSandalinas <146134950+JuanSandalinas@users.noreply.github.com>
2023-09-27 13:44:03 +0200

    Solution_JuanSandalinas.py

--- solution_JuanSandalinas.py
+++ solution_JuanSandalinas.py
@@ -5,7 +5,7 @@ import scipy.optimize as sciop
 import matplotlib.pyplot as plt
 
 "Read the csv file using ; as a delimiter betwen columns"
-df = pd.read_csv("C:/Users/joann/OneDrive/Escritorio/Intro/CS_Assignment/istherecorrelation.csv", sep = ";")
+df = pd.read_csv("istherecorrelation.csv", sep = ";")
 
 " Get columns of year,wo and nl of the data frame as a Series"
 years = df.iloc[:, 0].astype(int)
@@ -27,7 +27,7 @@ ax1.scatter(years, ratio, label = "Data")
 ax1.set_xticks(years)
 ax1.set_yticks(y_ticks)
 
-"Adding regression line analysis"
+"Adding regression line to the graph"
 def linear_function(x, m, n):
     return m * x + n
 params,covariance = sciop.curve_fit(linear_function, years, ratio)
@@ -36,8 +36,9 @@ y_fit = linear_function(years, m,n)
 ax1.plot(years, y_fit, color = "red", label = "Linear fit")
 
 "Adding legend of the graph"
-ax1.set_ylabel("Nl/WO beer consumption")
+ax1.set_ylabel("Nl/Wo beer consumption")
 ax1.set_xlabel("Year")
+plt.savefig('Beer.png', dpi=300) 
 
 
 
